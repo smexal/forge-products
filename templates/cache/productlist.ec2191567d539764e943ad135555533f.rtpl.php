655dfaa23a62c3ec5375ec7fb47242e618515901
Silas Mächler <silas.maechler@gmail.com>
2018-11-26 21:03:41 +0100

    no fixed height

--- templates/cache/productlist.ec2191567d539764e943ad135555533f.rtpl.php
+++ templates/cache/productlist.ec2191567d539764e943ad135555533f.rtpl.php
@@ -7,11 +7,17 @@
 
         <div class="col-lg-3 col-md-6 col-sm-12">
             <a class="product-card" <?php if( $value1["link"] ){ ?>href="<?php echo $value1["link"];?>"<?php } ?>>
-                <div class="img"><img src="<?php echo $value1["image"];?>" /></div>
+                <?php if( $value1["image"] ){ ?><div class="img"><img src="<?php echo $value1["image"];?>" /></div><?php } ?>
+
                 <div class="info">
                     <h3><?php echo $value1["title"];?></h3>
-                    <p><?php echo $value1["description"];?></p>
+                    <?php if( $value1["description"] ){ ?><p><?php echo $value1["description"];?></p><?php } ?>
+
                     <p class="price"><?php echo $value1["price"];?></p>
+                </div>
+                <div class="actions">
+                    <?php echo $value1["add_to_cart"];?>
+
                 </div>
             </a>
         </div>
